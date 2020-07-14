8630981f185d55a810cadf6ac0e6023a1a102ebc
Ludrak <luca06.robino@gmail.com>
2020-07-14 16:37:16 +0200

    Normed lib

--- vec.c
+++ vec.c
@@ -3,37 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   vec.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lrobino <lrobino@student.le-101.fr>        +#+  +:+       +#+        */
+/*   By: lrobino <lrobino@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/30 16:47:46 by lrobino           #+#    #+#             */
-/*   Updated: 2020/06/30 16:49:44 by lrobino          ###   ########lyon.fr   */
+/*   Created: 2020/07/14 16:20:33 by lrobino           #+#    #+#             */
+/*   Updated: 2020/07/14 16:21:00 by lrobino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vectors.h"
 
-t_vec2d     vec2d(double x, double y)
+t_vec2d		vec2d(double x, double y)
 {
-    return (create_vector(x, y));
+	return (create_vector(x, y));
 }
 
-t_vec2f     vec2f(float x, float y)
+t_vec2f		vec2f(float x, float y)
 {
-    return (create_vectorf(x, y));
+	return (create_vectorf(x, y));
 }
 
-t_vec2i     vec2i(int x, int y)
+t_vec2i		vec2i(int x, int y)
 {
-    return (create_vectori(x, y));
+	return (create_vectori(x, y));
 }
 
 /*
 **  UTIL
 */
-t_vec2d         vec_set_mag(t_vec2d vec, float mag)
+
+t_vec2d		vec_set_mag(t_vec2d vec, float mag)
 {
-    t_vec2d v;
-    v = vec_norm(vec);
-    v = vec_multf(vec, mag);
-    return (v);
+	t_vec2d v;
+
+	v = vec_norm(vec);
+	v = vec_multf(vec, mag);
+	return (v);
 }
--- vectors.h
+++ vectors.h
@@ -3,91 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   vectors.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lrobino <lrobino@student.le-101.fr>        +#+  +:+       +#+        */
+/*   By: lrobino <lrobino@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/09 13:58:40 by lrobino           #+#    #+#             */
-/*   Updated: 2020/06/30 16:47:05 by lrobino          ###   ########lyon.fr   */
+/*   Created: 2020/07/14 16:32:27 by lrobino           #+#    #+#             */
+/*   Updated: 2020/07/14 16:36:42 by lrobino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTORS_H
-#define VECTORS_H
+# define VECTORS_H
 
-# define PI		3.1415926535
-# define TWO_PI	2 * PI
-typedef struct  s_vec2d
-{
-    double x;
-    double y;
-}               t_vec2d;
+# ifndef PI
+#  define PI	3.1415926535f
+# endif
+# define TWO_PI	6.2831853071f
 
-typedef struct  s_vec2f
+typedef struct	s_vec2d
 {
-    float   x;
-    float   y;
-}               t_vec2f;
+	double	x;
+	double	y;
+}				t_vec2d;
 
-typedef struct  s_vec2i
+typedef struct	s_vec2f
 {
-    int     x;
-    int     y;
-}               t_vec2i;
+	float	x;
+	float	y;
+}				t_vec2f;
+
+typedef struct	s_vec2i
+{
+	int		x;
+	int		y;
+}				t_vec2i;
 
 /*
 **  VECTOR UTILS 2 DOUBLE
 */
-t_vec2d     vec2d(double x, double y);
-t_vec2d     create_vector(double x, double y);
-
-t_vec2d     vec_add(t_vec2d va, t_vec2d vb);
-
-t_vec2d     vec_sub(t_vec2d va, t_vec2d vb);
-
-t_vec2d     vec_mult(t_vec2d va, t_vec2d vb);
-
-t_vec2d     vec_multf(t_vec2d v, double amt);
-
-t_vec2d     vec_norm(t_vec2d vec);
-
-double      vec_mag(t_vec2d vec);
-
-double      vec_dot (t_vec2d va, t_vec2d vb);
-
-t_vec2d     vec_set_mag(t_vec2d vec, float mag);
-
+t_vec2d			vec2d(double x, double y);
+t_vec2d			create_vector(double x, double y);
+t_vec2d			vec_add(t_vec2d va, t_vec2d vb);
+t_vec2d			vec_sub(t_vec2d va, t_vec2d vb);
+t_vec2d			vec_mult(t_vec2d va, t_vec2d vb);
+t_vec2d			vec_multf(t_vec2d v, double amt);
+t_vec2d			vec_norm(t_vec2d vec);
+double			vec_mag(t_vec2d vec);
+double			vec_dot (t_vec2d va, t_vec2d vb);
+t_vec2d			vec_set_mag(t_vec2d vec, float mag);
 
 /*
 **  VECTOR UTILS 2 FLOAT
 */
-t_vec2f     vec2f(float x, float y);
-t_vec2f     create_vectorf(float x, float y);
-
-t_vec2f     vecf_add(t_vec2f va, t_vec2f vb);
-
-t_vec2f     vecf_sub(t_vec2f va, t_vec2f vb);
-
-t_vec2f     vecf_mult(t_vec2f va, t_vec2f vb);
-
-t_vec2f     vecf_norm(t_vec2f vec);
-
-float      vecf_mag(t_vec2f vec);
-
-float      vecf_dot (t_vec2f va, t_vec2f vb);
+t_vec2f			vec2f(float x, float y);
+t_vec2f			create_vectorf(float x, float y);
+t_vec2f			vecf_add(t_vec2f va, t_vec2f vb);
+t_vec2f			vecf_sub(t_vec2f va, t_vec2f vb);
+t_vec2f			vecf_mult(t_vec2f va, t_vec2f vb);
+t_vec2f			vecf_norm(t_vec2f vec);
+float			vecf_mag(t_vec2f vec);
+float			vecf_dot (t_vec2f va, t_vec2f vb);
 
 /*
 **  VECTOR UTILS 2 INT
 */
-t_vec2i     vec2i(int x, int y);
-t_vec2i     create_vectori(int x, int y);
 
-t_vec2i     veci_add(t_vec2i va, t_vec2i vb);
-
-t_vec2i     veci_sub(t_vec2i va, t_vec2i vb);
-
-t_vec2i     veci_mult(t_vec2i va, t_vec2i vb);
-
-t_vec2i     veci_norm(t_vec2i vec);
-
-int         veci_mag(t_vec2i vec);
+t_vec2i			vec2i(int x, int y);
+t_vec2i			create_vectori(int x, int y);
+t_vec2i			veci_add(t_vec2i va, t_vec2i vb);
+t_vec2i			veci_sub(t_vec2i va, t_vec2i vb);
+t_vec2i			veci_mult(t_vec2i va, t_vec2i vb);
+t_vec2i			veci_norm(t_vec2i vec);
+int				veci_mag(t_vec2i vec);
 
 #endif
